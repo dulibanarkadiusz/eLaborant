bd4ff106c0095f9dfe376f446697dd82021478ce
dulibanarkadiusz <arkadiusz.duliban@gmail.com>
2017-07-02 00:53:53 +0200

    Changes in project structure

--- js/elaborantApp.js
+++
@@ -1,9 +0,0 @@
-var elaborantApp = angular.module('elaborant', ["ui.router"]);
-
-elaborantApp.controller('usersList', function ($scope, $http) {
-    $scope.dataLoaded = false;
-    $http.get('http://serwis365.pl/api/getFreeTime.php').success(function (response) {
-        $scope.myData = response;
-        $scope.dataLoaded = true;
-    });
-});
