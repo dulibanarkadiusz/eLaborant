c608a6a8abac038b719093d3399004c584bc799e
dulibanarkadiusz <arkadiusz.duliban@gmail.com>
2017-07-02 16:38:01 +0200

    Computer page + links

--- js/controllers.js
+++ js/controllers.js
@@ -329,6 +329,56 @@
         $scope.loadData();
     }); 
 
+    var computerProblemsData;
+    elaborantApp.controller('computer', function($scope, $sce, amMoment, $stateParams, $http) {
+        $scope.computerid = $stateParams.id;
+        $scope.computerDataLoaded = false;
+        $scope.pageSize = (localStorage.pageSize) ? parseInt(localStorage.pageSize) : defaultPageSize;
+        $scope.pages = [];
+        $scope.errorDataLoaded = '';
+        $scope.problemsCount = 0;
+        amMoment.changeLocale('pl');
+
+        $scope.loadData = function() {
+            $scope.message = "";
+            $http.get(apiUrl + 'computers/'+$scope.computerid).success(function (serverResponse) {
+                $scope.computerData = new Array(serverResponse.response);
+                $scope.computerDataLoaded = true;
+            })
+            .error(function(error, status){
+                if (status==404){
+                    $scope.errorDataLoaded = $sce.trustAsHtml(parseErrorInfo('(404) Komputer nie został znaleziony.'));
+                }
+                else{
+                    $scope.errorDataLoaded = $sce.trustAsHtml(parseErrorInfo(dataError));
+                }
+                return;
+            });
+   
+            computerProblemsData = function(pageNumber = 0) { 
+                $http.get(apiUrl + 'problems/?query=idComputer%3D'+$scope.computerid+',page=' + pageNumber + ",pageSize=" + $scope.pageSize)
+                    .success(function (serverResponse) {
+                        $scope.problemsCount = serverResponse.totalElements;
+                        $scope.problemsData = serverResponse.response;
+                        $scope.dataLoaded = true;
+                        $scope.pages = getPagesArray(serverResponse.totalPages);
+                        $scope.currentPage = pageNumber;
+                        localStorage.pageSize = $scope.pageSize;
+                    })
+                    .error(function(error, status) {
+                        switch(status){
+                            case 404: 
+                                $scope.message = "Brak zgłoszonych problemów dla tego komputera";
+                            break;
+                        }
+                    });
+            }
+            computerProblemsData();
+        }
+
+        $scope.loadData();
+    }); 
+
 
     elaborantApp.directive('bsTooltip', function(){
         return {
