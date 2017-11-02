a6ec077eac750f0d71ced6a5eb354966df00bb1f
mwolszleger <mwolszleger@interia.pl>
2017-11-02 23:09:55 +0100

    added computers edition

--- app/components/Computer/ComputerCtrl.js
+++ app/components/Computer/ComputerCtrl.js
@@ -7,68 +7,78 @@ angular.module('elaborantComputerCtrl', []).controller('ComputerCtrl', function(
     $scope.problemsCount = 0;
     amMoment.changeLocale('pl');
 
-    $scope.addNewComputer = function(){ 
+    $scope.addNewComputer = function(computerId = null){ 
         var modalInstance = $modal.open({
             templateUrl: 'modals/addComputerView.html',
-            controller: 'addComputerFormController'
+            controller: 'ComputerManagerCtrl',
+            backdrop: 'static',
+            resolve: {
+                param: function(){
+                    return {'id':computerId}
+                }
+            }
         });
     };
 
-    $rootScope.$on("RefreshList", function(){
-        $scope.getList();
-    });
-
-    $scope.getList = function(pageNumber = 0) {
-        $http.get(apiUrl + 'computers?query=page=' + pageNumber + ",pageSize=" + $scope.pageSize)
-        .success(function (serverResponse) {
-            $scope.computersListData = serverResponse.response;
-            $scope.dataLoaded = true;
-            $scope.totalElements = serverResponse.totalElements;
-            $scope.pages = getPagesArray(serverResponse.totalPages);
-            $scope.currentPage = pageNumber;
-            localStorage.pageSize = $scope.pageSize;
-        })
-        .error(function(data, status){
-            $scope.responseError = true;
-            $scope.errorMessage = $sce.trustAsHtml(errorMessage);
-        });
-    };
-
-    $scope.getEntity = function(idComputer = $scope.computerid) {
-        $scope.message = "";
-        $http.get(apiUrl + 'computers/'+$scope.computerid).success(function (serverResponse) {
-            $scope.computerData = new Array(serverResponse.response);
-            $scope.computerDataLoaded = true;
-        })
-        .error(function(error, status){
-            if (status==404){
-                $scope.errorDataLoaded = $sce.trustAsHtml(parseErrorInfo('(404) Komputer nie został znaleziony.'));
-            }
-            else{
-                $scope.errorDataLoaded = $sce.trustAsHtml(parseErrorInfo(dataError));
-            }
-            return;
+        $rootScope.$on("RefreshList", function(){
+            $scope.getList();
         });
 
-        /* Zgłoszone problemy dla komputera 
-        computerProblemsData = function(pageNumber = 0) { 
-            $http.get(apiUrl + 'problems/?query=idComputer%3D'+$scope.computerid+',page=' + pageNumber + ",pageSize=" + $scope.pageSize)
-                .success(function (serverResponse) {
-                    $scope.problemsCount = serverResponse.totalElements;
-                    $scope.problemsData = serverResponse.response;
-                    $scope.dataLoaded = true;
-                    $scope.pages = getPagesArray(serverResponse.totalPages);
-                    $scope.currentPage = pageNumber;
-                    localStorage.pageSize = $scope.pageSize;
+        $scope.getList = function(pageNumber = 0) {
+            $http.get(apiUrl + 'computers?query=page=' + pageNumber + ",pageSize=" + $scope.pageSize)
+            .success(function (serverResponse) {
+                $scope.computersListData = serverResponse.response;
+                $scope.dataLoaded = true;
+                $scope.totalElements = serverResponse.totalElements;
+                $scope.pages = getPagesArray(serverResponse.totalPages);
+                $scope.currentPage = pageNumber;
+                localStorage.pageSize = $scope.pageSize;
+            })
+            .error(function(data, status){
+                $scope.responseError = true;
+                $scope.errorMessage = $sce.trustAsHtml(errorMessage);
+            });
+        };
+
+            $scope.getEntity = function(idComputer = $scope.computerid) {
+                $scope.message = "";
+                $http.get(apiUrl + 'computers/'+$scope.computerid).success(function (serverResponse) {
+                    $scope.computerData = new Array(serverResponse.response);
+                    $scope.computerDataLoaded = true;
                 })
-                .error(function(error, status) {
-                    switch(status){
-                        case 404: 
-                            $scope.message = "Brak zgłoszonych problemów dla tego komputera";
-                        break;
+                .error(function(error, status){
+                    if (status==404){
+                        $scope.errorDataLoaded = $sce.trustAsHtml(parseErrorInfo('(404) Komputer nie został znaleziony.'));
                     }
+                    else{
+                        $scope.errorDataLoaded = $sce.trustAsHtml(parseErrorInfo(dataError));
+                    }
+                    return;
                 });
-        }
-        computerProblemsData();*/
-    }
-}); 
+
+                /* Zgłoszone problemy dla komputera 
+                computerProblemsData = function(pageNumber = 0) { 
+                    $http.get(apiUrl + 'problems/?query=idComputer%3D'+$scope.computerid+',page=' + pageNumber + ",pageSize=" + $scope.pageSize)
+                        .success(function (serverResponse) {
+                            $scope.problemsCount = serverResponse.totalElements;
+                            $scope.problemsData = serverResponse.response;
+                            $scope.dataLoaded = true;
+                            $scope.pages = getPagesArray(serverResponse.totalPages);
+                            $scope.currentPage = pageNumber;
+                            localStorage.pageSize = $scope.pageSize;
+                        })
+                        .error(function(error, status) {
+                            switch(status){
+                                case 404: 
+                                    $scope.message = "Brak zgłoszonych problemów dla tego komputera";
+                                break;
+                            }
+                        });
+                }
+                computerProblemsData();*/
+            }
+                $scope.editComputer = function(computerId){
+	
+                    $scope.addNewComputer(computerId);
+                }
+            }); 
