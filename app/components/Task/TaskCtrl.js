b8efd2338e357f3eaa675fe4d6c64c03d90992a0
Unknown <arkadiuszduliban@gmail.com>
2017-11-13 22:11:35 +0100

    Task uses Services. Improvements.

--- app/components/Task/TaskCtrl.js
+++ app/components/Task/TaskCtrl.js
@@ -1,4 +1,4 @@
-angular.module('elaborantTaskCtrl', []).controller('TaskCtrl', function ($scope, $rootScope, $injector, $sce, amMoment, $stateParams, $http, $modal, ModalService) {
+angular.module('elaborantTaskCtrl', []).controller('TaskCtrl', function ($scope, $rootScope, $injector, $sce, amMoment, $stateParams, $http, $modal, ModalService, NotificationService) {
     amMoment.changeLocale('pl');
     $scope.pageSize = (localStorage.pageSize) ? parseInt(localStorage.pageSize) : defaultPageSize;
     $scope.pages = [];
@@ -7,29 +7,20 @@ angular.module('elaborantTaskCtrl', []).controller('TaskCtrl', function ($scope,
         $scope.getList();
     });
     
-
     $scope.getList = function(pageNumber = 0, problemId = $stateParams.id){
         $scope.dataLoaded = false;
         $scope.message = null;
-        var problemIdQuery = (typeof problemId === "undefined") ? '' : 'idProblem%3D'+problemId+',';
-        $http.get(apiUrl + 'tasks/?query='+problemIdQuery+'page=' + pageNumber + ",pageSize=" + $scope.pageSize)
-        .success(function (serverTaskResponse) {
-            $scope.taskData = serverTaskResponse.response;
-            $scope.tasksCount = serverTaskResponse.totalElements;
-            $scope.pages = getPagesArray(serverTaskResponse.totalPages);
-            $scope.currentPage = pageNumber;
-            localStorage.pageSize = $scope.pageSize;
-            
-            $scope.dataLoaded = true;
-            for (var i = 0; i < $scope.taskData.length; i++ ){
-                var task = $scope.taskData[i];
-                task.executorsString = "";
-                for (var j = 0; j < task.userExecuteTasksById.length; j++ ){
-                    task.executorsString += task.userExecuteTasksById[j].firstname + " " + task.userExecuteTasksById[j].surname + "\n";
-                }
-            }
+        var problemIdQuery = (typeof problemId === "undefined") ? '' : 'idProblem%3D'+problemId+',';    // creates query added to URL
+        
+        $http({
+            method: 'GET',
+            url: apiUrl + 'tasks/?query='+problemIdQuery+'page=' + pageNumber + ",pageSize=" + $scope.pageSize,
         })
-        .error(function(error, status) {
+        .then(function (response) {
+            $scope.currentPage = pageNumber;
+            DisplayTasksList(response);
+        },
+        function (response) {
             $scope.messageType = messageType.Error;
             switch(status){
                 case 403:
@@ -62,4 +53,18 @@ angular.module('elaborantTaskCtrl', []).controller('TaskCtrl', function ($scope,
         var modalInstance = $modal.open(options);
     }
 
+    function DisplayTasksList(serverTaskResponse) { 
+        $scope.taskData = serverTaskResponse.data.response;
+        $scope.tasksCount = serverTaskResponse.totalElements;
+        $scope.pages = getPagesArray(serverTaskResponse.totalPages);
+        $scope.dataLoaded = true;
+        
+        for (var i = 0; i < $scope.taskData.length; i++ ){
+            var task = $scope.taskData[i];
+            task.executorsString = "";
+            for (var j = 0; j < task.userExecuteTasksById.length; j++ ){
+                task.executorsString += task.userExecuteTasksById[j].firstname + " " + task.userExecuteTasksById[j].surname + "\n";
+            }
+        }
+    }
 });
