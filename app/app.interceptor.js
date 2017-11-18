4c4c42fbabdeedbc53c2a3e98e57250388a3e3c4
mwolszleger <mwolszleger@interia.pl>
2017-11-18 23:06:24 +0100

    refresh tokens

--- app/app.interceptor.js
+++ app/app.interceptor.js
@@ -1,12 +1,32 @@
-elaborantApp.factory('httpRequestInterceptor', function () {
-  return {
-    request: function (config) {
-      config.headers['Authorization'] = localStorage.getItem('token'); 
-      return config;
-    }
-  };
+elaborantApp.factory('httpRequestInterceptor', function ($q, $injector) {
+    return {
+        request: function (config) {
+            config.headers['Authorization'] = localStorage.getItem('token');
+            return config;
+        },
+        responseError: function (response) {
+            var LoginService = $injector.get('LoginService');
+
+            if (response.status === 401 && LoginService.isLogged()) {
+
+                var deferred = $q.defer();
+
+                LoginService.refresh(function (result) {
+                    if (result.success) {
+                        $injector.get("$http")(response.config).then(function (resp) { deferred.resolve(resp); });
+                    }
+                    else {
+                        deferred.reject();
+                        alert("Wystąpił błąd");
+                    }
+                });
+                return deferred.promise;
+            }
+            return response;
+        }
+    };
 });
 
 elaborantApp.config(function ($httpProvider) {
-  $httpProvider.interceptors.push('httpRequestInterceptor');
+    $httpProvider.interceptors.push('httpRequestInterceptor');
 });
